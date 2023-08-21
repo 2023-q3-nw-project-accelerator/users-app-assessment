57f710c8894804e9e39b39c8958492f73fc201ce
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2023-08-21 14:53:37 -0400

    fetch data from external API

--- src/App.js
+++ src/App.js
@@ -2,8 +2,31 @@ import SearchBar from './components/SearchBar/SearchBar';
 import Users from './components/Users/Users';
 import './App.css';
 
+import { useEffect, useState } from 'react';
+ 
+
+
 function App() {
+  const [users, setUsers] = useState([])
+
+  const API_URL = "https://users-app-backend.onrender.com/users"
   // TODO: Fetch data here
+
+  useEffect(() => {
+    console.log(`<App/> useEffect FIRED`)
+
+    async function fetchData() {
+      const response = await fetch(`${API_URL}`)
+      const json =  await response.json()
+        console.log(`<App/> useEffect() fetch data`, json)
+
+       const {data} = json
+       setUsers(data) 
+      
+    }
+    fetchData()
+  },[])
+ 
 
   return (
     <div className="App">
