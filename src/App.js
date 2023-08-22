1007bd9b66996d14e4299260ef1510ea3ea6a548
Tech-Kat <katpelle@pursuit.org>
2023-08-22 16:56:41 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,9 +1,49 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const API_URL = "https://users-app-backend.onrender.com/users";
 
 function App() {
-  // TODO: Fetch data here
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        setError("");
+        setLoading(true);
+        const response = await fetch(`${API_URL}`);
+        const json = await response.json();
+        const { error, data } = json;
+
+        if (response.ok) {
+          setUserData(data);
+          setLoading(false);
+        } else {
+          setError(error);
+          setLoading(false);
+        }
+      } catch (err) {
+        setLoading(false);
+        setError(err.message);
+      }
+    }
+    fetchData();
+  }, []);
+
+  const renderContent = () => {
+    if(loading){
+      return 
+    } else if(error) {
+return
+    } else {
+      return
+    }
+  }
 
   return (
     <div className="App">
