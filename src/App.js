4797cdb6211fdb16641aaed75be83e0477bda9d4
Wwright91 <wisdomwright-martin@pursuit.org>
2023-08-21 20:57:20 -0400

    Fetch made to user data
    1. Added .env file
    2. Fetch to external API was successful
    3. State created for user data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+import { useState, useEffect } from "react";
+
+const API_URL = process.env.REACT_APP_API_URL;
 
 function App() {
-  // TODO: Fetch data here
+  const [userData, setUserData] = useState([]);
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`${API_URL}/users`);
+        const json = await response.json();
+        const { data } = json;
+        if (response.ok) {
+          console.log(data);
+          setUserData(data);
+        }
+      } catch (err) {
+        console.log(`<App/> useEffect error: ${err.message}`);
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
