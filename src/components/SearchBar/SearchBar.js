832766a32e05fe22d019081d462a0dd227131153
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2023-08-22 17:59:57 -0400

    SearchBar input functionality

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
 import './SearchBar.css';
 
-const SearchBar = () => {
+const SearchBar = ({users}) => {
+  //create useState for user input
+  const [searchInput, setSearchInput] = useState('')
+
+  //input value is updated when user types in search bar
+  const handleChange = (e) =>{
+    setSearchInput(e.target.value)
+  }
+
+ 
+  console.log(`<SearchBar/> Rendered searchInput = ${searchInput}`)
+
   return (
-    <input type="text" placeholder="Search by name, country, or company" />
+    <input 
+    //set value to dynamic user input
+    value={searchInput}
+    type="text" 
+    placeholder="Search by name, country, or company"
+    onChange={handleChange} 
+    />
   );
 };
 
--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,30 +1,19 @@
-import User from '../User/User';
-import './Users.css';
-import SearchBar from '../SearchBar/SearchBar';
+import User from "../User/User";
+import "./Users.css";
+import SearchBar from "../SearchBar/SearchBar";
 
 const Users = ({ users = [] }) => {
-  console.log(`Users Rendered!`)
-
-// let dataToDisplay = users
-
-// dataToDisplay = users.filter((user) => {
-//   const name = `${name}`.toLowerCase()
-//   const country = `${country}`.toLowerCase()
-//   const company = `${company}`.toLowerCase()
-
-//   return 
-// })
-
+  console.log(`Users Rendered!`);
 
   return (
-    <div className='Users__searchbar'>
-    <SearchBar/>
-    <article className="Users">
-      {users.map((user) => {
-        const { id } = user;
-        return <User key={id} user={user} />;
-      })}
-    </article>
+    <div className="Users__searchbar">
+      <SearchBar />
+      <article className="Users">
+        {users.map((user) => {
+          const { id } = user;
+          return <User key={id} user={user} />;
+        })}
+      </article>
     </div>
   );
 };
