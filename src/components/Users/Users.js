ffd1d280c417d21bd8f6d22a02ddc1de467c03af
Wwright91 <wisdomwright-martin@pursuit.org>
2023-08-21 22:44:26 -0400

    Show More/Less Button Implemented
    1. Created expanded state in Users component
    2. Created function to toggle which users are expanded
    3. Pass props to User component
    4. Implement ternary statement for button to display Show More or Show Less
    5. Created logic to show/hide about me section

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,13 +1,35 @@
+import { useState } from "react";
 import User from "../User/User";
 import "./Users.css";
 
 const Users = ({ input, userData }) => {
+  const [expanded, setExpanded] = useState([]);
+
+  const handleToggleExpanded = (id) => {
+    if (!expanded.includes(id)) {
+      const newExpanded = [...expanded, id];
+      setExpanded(newExpanded);
+    } else {
+      const removed = expanded.filter((currId) => currId !== id);
+      setExpanded(removed);
+    }
+  };
+
+  console.log("expanded", expanded);
+
   return (
     <article className="Users">
       {userData.length
         ? userData.map((user) => {
             const { id } = user;
-            return <User key={id} user={user} />;
+            return (
+              <User
+                key={id}
+                user={user}
+                expanded={expanded.includes(user.id)}
+                onClick={() => handleToggleExpanded(user.id)}
+              />
+            );
           })
         : `No results for ${input}!`}
     </article>
