c47973f23798857743fc7b93c8902403922cc765
Younes Tahiri <you@example.com>
2023-08-24 10:21:26 -0400

    Initialize state for error,loading,userData.
    This commit establishes the foundational state management for the
    application. Upon fetching user data from the API, the response status
    is evaluated. If the response status indicates success, the user data
    state is updated accordingly. Conversely, if an error occurs, the error
    state is set to provide appropriate feedback. The loading state ensures
    that users are informed while data is being fetched.
    
    future commits this implementation prepares the application to render
    different outcomes based on the state conditions. Depending on
    the truthiness of each state (users, error, loading), the user
    interface will display corresponding messages or content.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import axios from "axios";
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
-  // TODO: Fetch data here
+  const [userData, setUserData] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState("");
+
+  useEffect(() => {
+    const PORT = process.env.REACT_APP_API_URL;
+
+    // Fetch users data
+    axios
+      .get(PORT)
+      .then((res) => {
+        const responseIsOk = res.status === 200;
+        const data = res.data;
+
+        // Check if response status is ok
+        if (responseIsOk) {
+          console.log(data);
+          // handle success.
+          setUserData(data);
+        } else {
+          // handle error.
+          setError(error);
+        }
+        //stop showing the user the loading UI...
+        setLoading(false);
+      })
+      .catch((error) => {
+        // handle error.
+        setError(error);
+        setLoading(false);
+      });
+  }, []);
 
   return (
     <div className="App">
