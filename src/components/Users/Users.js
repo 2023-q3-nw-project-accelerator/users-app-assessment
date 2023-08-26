79ba5581206d4a3da2653b474ba22258ed095224
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2023-08-25 22:45:04 -0400

    Users styling

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,13 +1,21 @@
 import User from "../User/User";
 import "./Users.css";
 import SearchBar from "../SearchBar/SearchBar";
+import { useState } from "react";
 
 const Users = ({ users = [] }) => {
   console.log(`Users Rendered!`);
+  
+  const [expanded, setExpanded] = useState(false)
 
   return (
-    <div className="Users__searchbar">
-      <SearchBar />
+    <div className="Users__controls">
+    <span>
+    <SearchBar />
+    <button onClick={(expanded)}>Expand All</button>
+    <button onClick={(!expanded)}>Collapse All</button>
+    </span>
+      
       <article className="Users">
         {users.map((user) => {
           const { id } = user;
