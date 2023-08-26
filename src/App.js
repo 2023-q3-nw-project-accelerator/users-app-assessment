ceb8ff26a2dc69500b29c757c1781f214fadebf8
ziqian pan <ziqianpan1998@gmail.com>
2023-08-25 22:42:06 -0400

    fetched data from external api

--- src/App.js
+++ src/App.js
@@ -1,9 +1,34 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
+import { useState, useEffect } from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+
+const API_URL = "https://users-app-backend.onrender.com";
 
 function App() {
+  const [userData, setUserData] = useState([]);
   // TODO: Fetch data here
+
+  useEffect(() => {
+    async function fetchData() {
+      try {
+        const response = await fetch(`${API_URL}/users`);
+        const json = await response.json();
+        // console.log(json.data);
+        const { data, error } = json;
+        if (response.ok) {
+        setUserData(data);
+        } else {
+          // error handling
+        }
+
+
+      } catch (err) {
+        //update error 
+      }
+    }
+    fetchData();
+  }, []);
 
   return (
     <div className="App">
