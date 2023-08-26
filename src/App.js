d31f4b98848b0e0ff95cfcd690e622827545eb00
Bo Latt <bolattny@gmail.com>
2023-08-26 14:23:50 -0400

    fetch data, add loading and error state, implement search bar

--- src/App.js
+++ src/App.js
@@ -1,15 +1,44 @@
-import SearchBar from './components/SearchBar/SearchBar';
-import Users from './components/Users/Users';
-import './App.css';
-
+import { useState } from "react";
+import SearchBar from "./components/SearchBar/SearchBar";
+import Users from "./components/Users/Users";
+import "./App.css";
+import useAxios from "./hooks/useAxios";
 function App() {
-  // TODO: Fetch data here
+  const API_URL = "https://users-app-backend.onrender.com/users";
+  const { data, loading, error } = useAxios(API_URL);
+  const [input, setInput] = useState("");
 
+  console.log(data, loading, "error ", error);
+  function filterUsers() {
+    return data.filter((user) => {
+      return (
+        user.name.toLowerCase().includes(input.toLowerCase()) ||
+        user.country.toLowerCase().includes(input.toLowerCase()) ||
+        user.company.toLowerCase().includes(input.toLowerCase())
+      );
+    });
+  }
+  console.log(input);
+  function renderContent() {
+    if (loading) {
+      return <p>Loading...</p>;
+    }
+    if (error) {
+      return <p>{error}</p>;
+    }
+    if (data) {
+      return (
+        <>
+          <SearchBar value={input} onChange={setInput} />
+          <Users users={filterUsers()} />;
+        </>
+      );
+    }
+  }
   return (
     <div className="App">
       <h1>Our Users</h1>
-      <SearchBar />
-      <Users />
+      {renderContent()}
     </div>
   );
 }
---
+++ src/hooks/useAxios.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+
+export default function useAxios(url) {
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      setError("");
+      try {
+        const response = await axios.get(url);
+        setData(response.data.data);
+      } catch (error) {
+        setError(error.message);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return {
+    data,
+    loading,
+    error,
+  };
+}
