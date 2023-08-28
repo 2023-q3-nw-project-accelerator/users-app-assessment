a84f0ce1052483ed92a5a23d1251d680aad02b1c
Kenyetta Griffin (She/Her) <96461060+PursuitMadeMe@users.noreply.github.com>
2023-08-27 21:09:23 -0400

    'filter bug'

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -4,23 +4,49 @@ import './SearchBar.css';
 const SearchBar = ({users}) => {
   //create useState for user input
   const [searchInput, setSearchInput] = useState('')
+  const [expand, setExpanded] = useState(false)
 
-  //input value is updated when user types in search bar
-  const handleChange = (e) =>{
+
+  const handleChange = (e) => {
     setSearchInput(e.target.value)
   }
 
- 
-  console.log(`<SearchBar/> Rendered searchInput = ${searchInput}`)
+  
+
+//SEARCH FILTER BUG STUCKKKK
+
+  let dataToDisplay = users
+
+// if(searchInput){
+//   dataToDisplay = users.filter((user) =>{
+//     const {name} = user
+//     const {company} = user.company
+//     const {country} = user.country
+
+//     return company.includes(searchInput)
+//   })
+// }
+
+
+
+
+
+  console.log(`<SearchBar/> Rendered searchInput = ${searchInput}  `)
 
   return (
+    <div className='SearchBar--controls'>
+    <span>
     <input 
-    //set value to dynamic user input
     value={searchInput}
     type="text" 
     placeholder="Search by name, country, or company"
     onChange={handleChange} 
     />
+    <button onClick={() =>{setExpanded(!expand)}}>Expand All</button>
+    <button>Collapse All</button>
+    </span>
+
+    </div>
   );
 };
 
