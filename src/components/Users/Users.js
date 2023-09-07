9122fae05925f268233c73de85bcb41c31f59600
Lennie Nurse <lnursej@gmail.com>
2023-09-07 16:57:50 -0400

    added NoContent component

--- src/components/Users/Users.js
+++ src/components/Users/Users.js
@@ -1,22 +1,25 @@
 import User from "../User/User"
 import "./Users.css"
+import NoContent from "../NoContent/NoContent"
 
 const Users = ({ input, toggleExpand, expanded, users = [] }) => {
   return (
     <article className="Users">
-      {users.length
-        ? users.map((user) => {
-            const { id } = user
-            return (
-              <User
-                key={id}
-                user={user}
-                expanded={expanded.includes(user.id)}
-                toggleExpand={() => toggleExpand(user.id)}
-              />
-            )
-          })
-        : `no results for: ${input}`}
+      {users.length ? (
+        users.map((user) => {
+          const { id } = user
+          return (
+            <User
+              key={id}
+              user={user}
+              expanded={expanded.includes(user.id)}
+              toggleExpand={() => toggleExpand(user.id)}
+            />
+          )
+        })
+      ) : (
+        <NoContent input={input} />
+      )}
     </article>
   )
 }
